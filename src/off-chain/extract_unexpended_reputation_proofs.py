751ebfd6c073bfddd71509be90085acb1997c81b
jossemii <jossemii@proton.me>
2023-10-14 20:09:51 +0200

    extract_unexpended_reputation_proofs.py demo only

--- src/off-chain/extract_unexpended_reputation_proofs.py
+++ src/off-chain/extract_unexpended_reputation_proofs.py
@@ -1,6 +1,7 @@
 #  Extract the reputation proofs of an owner that can be expended.
 from typing import List
 from random import randrange
+from hashlib import sha512
 from ergpy import appkit, helper_functions
 
 
@@ -10,15 +11,15 @@ class ReputationProofs:
         self.box_id = box_id
         self.token_id = token_id
         self.total_amount = total_amount
-        self.expended_amount = expended_amount
-        self.free_percentage = ((1 - expended_amount) / total_amount) * 100
+        self.free_amount = total_amount - expended_amount
+        self.free_percentage = round((self.free_amount / total_amount) * 100, 2)
 
     def __dict__(self):
         return {
             "box_id": self.box_id.decode("utf-8"),
             "token_id": self.token_id.decode("utf-8"),
             "total_amount": self.total_amount,
-            "expended_amount": self.expended_amount,
+            "free_amount": self.free_amount,
             "free_percentage": self.free_percentage
         }
 
@@ -35,9 +36,11 @@ def extract_unexpended_reputation_proofs(owner_pk: bytes) -> List[ReputationProo
 
     return [
         ReputationProofs(
-            box_id=b"n2kl321k32j3k12j3klj12kl3jk12j2kl3jkl",
-            token_id=b"fldfjad9fdaf8da08f0da98f098ad0f809ad8",
-            total_amount=randrange(110000, 100000000000),
-            expended_amount=randrange(0, 110000)
+            # box_id=sha512(randrange(0, 10)).digest(), TODO
+            # token_id=sha512(randrange(0, 10)).digest(),
+            box_id=b"dfkjdaljd",
+            token_id=b"ff9d7f8ad8979",
+            total_amount=randrange(50, 100),
+            expended_amount=randrange(0, 50)
         ) for _i in range(10)
     ]
