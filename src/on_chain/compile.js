81c08b516a099403835c429c7c25bf2e3a3e4492
jossemii <jossemii@proton.me>
2023-10-29 15:42:54 +0100

    structure change

--- src/on_chain/compile.js
+++
@@ -1,6 +0,0 @@
-import { compile } from "@fleet-sdk/compiler";
-import fs from 'fs';
-
-const contract =  fs.readFileSync('src/on_chain/reputation_proof.scala', 'utf8');
-const contractAddress = compile(contract).toAddress().toString()
-console.log(contractAddress)
