bc0b44b81dca8ac2b716facfdbbd10fcc2ebec41
jossemii <jossemii@proton.me>
2023-10-29 20:40:20 +0100

    some improvements of the modal and form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-    import { TransactionBuilder, OutputBuilder } from "@fleet-sdk/core";
+    import Modal from './Modal.svelte';
+    let showModal = false;
 
-    interface Ergo {
-        get_change_address(): string;
-        get_current_height(): number;
-        get_utxos(): Box<any>[];
-        sign_tx(unsignedTransaction: any): Promise<any>; // Replace 'any' with the actual type of unsignedTransaction
-        submit_tx(signedTransaction: any): Promise<string>; // Replace 'any' with the actual type of signedTransaction
-    }
+    let connected = false;
 
     async function connectNautilus()
     {
@@ -15,7 +10,7 @@
           const nautilus = ergoConnector.nautilus;
           if (nautilus) { // check if Nautilus Wallet is available
             console.log("Nautilus Wallet is ready to use");
-            const connected = await nautilus.connect();
+            connected = await nautilus.connect();
             if (connected) {
               console.log("Connected!");
             } else {
@@ -28,57 +23,17 @@
           console.log("No wallet available");
         }
     }
-
-    async function generate_reputation_proof(new_one: boolean, token_id: string, token_amount: string) {
-        /*
-              Once the connection request is accepted by the user, this API will be injected in the same
-              way as the Connection API, and you can interact with it through the ergo object.
-         */
-        let ergo: Ergo;
-        const wallet_pk = ergo.get_change_address();
-
-        const token_label: string = calculateSHA256(wallet_pk + ergo.get_current_height().toString());  // TODO set by user optionally.
-        const builder = new OutputBuilder(
-          1000n,
-          wallet_pk
-        );
-
-        if (new_one) {
-          // https://fleet-sdk.github.io/docs/transaction-building#step-4-2-mint-a-token
-          builder.mintToken({
-            amount: token_amount, // the amount of tokens being minted without decimals
-            name: "reputation-token-" + token_label, // the name of the token
-            decimals: 0, // the number of decimals
-            description: "Reputation token "  + token_label + " of the wallet " + wallet_pk
-          })
-        } else {
-          // https://fleet-sdk.github.io/docs/transaction-building#step-4-1-add-tokens
-          builder.addTokens({
-            tokenId: token_id,
-            amount: token_amount
-          }, {sum: false})
-        }
-
-        // TODO assign the contract.
-        const unsignedTransaction = new TransactionBuilder(await ergo.get_current_height())
-          .from(ergo.get_utxos()) // add inputs
-          .to(builder)
-          .sendChangeTo(wallet_pk) // set change address
-          .payMinFee() // set fee
-          .build() // build!
-          .toPlainObject();
-
-        const signedTransaction = await ergo.sign_tx(unsignedTransaction);
-        const transactionId = await ergo.submit_tx(signedTransaction);
-        console.log("transaction id -> ", transactionId)
-    }
-
 </script>
 
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to the Reputation System Panel</h1>
 
 
 
 <button on:click={connectNautilus}>Connect to Nautilus</button>
-<button on:click={connectNautilus}>Generate Reputation Proof</button>
+
+{#if connected}
+    <button on:click={() => (showModal = true)}> Generate reputation proof </button>
+
+    <Modal bind:showModal/>
+{/if}
---
+++ src/routes/Modal.svelte
@@ -0,0 +1,165 @@
+<script lang="ts">
+	import {OutputBuilder, TransactionBuilder} from "@fleet-sdk/core";
+
+    async function generate_reputation_proof(new_one: boolean, token_id: string, token_amount: string) {
+        /*
+              Once the connection request is accepted by the user, this API will be injected in the same
+              way as the Connection API, and you can interact with it through the ergo object.
+         */
+        console.log("new one ", new_one, "   token id ", token_id, "   token_amount", token_amount)
+
+        const wallet_pk = ergo.get_change_address();
+
+        const token_label: string = wallet_pk + ergo.get_current_height().toString();
+        const builder = new OutputBuilder(
+          1000n,
+          wallet_pk
+        );
+
+        if (new_one) {
+          // https://fleet-sdk.github.io/docs/transaction-building#step-4-2-mint-a-token
+          builder.mintToken({
+            amount: token_amount, // the amount of tokens being minted without decimals
+            name: "reputation-token-" + token_label, // the name of the token
+            decimals: 0, // the number of decimals
+            description: "Reputation token "  + token_label + " of the wallet " + wallet_pk
+          })
+        } else {
+          // https://fleet-sdk.github.io/docs/transaction-building#step-4-1-add-tokens
+          builder.addTokens({
+            tokenId: token_id,
+            amount: token_amount
+          }, {sum: false})
+        }
+
+        // TODO assign the contract.
+        const unsignedTransaction = new TransactionBuilder(await ergo.get_current_height())
+          .from(ergo.get_utxos()) // add inputs
+          .to(builder)
+          .sendChangeTo(wallet_pk) // set change address
+          .payMinFee() // set fee
+          .build() // build!
+          .toPlainObject();
+
+        const signedTransaction = await ergo.sign_tx(unsignedTransaction);
+        const transactionId = await ergo.submit_tx(signedTransaction);
+        console.log("transaction id -> ", transactionId)
+    }
+
+
+	export let showModal; // boolean
+
+	let dialog; // HTMLDialogElement
+
+	$: if (dialog && showModal) dialog.showModal();
+
+	let selectedOption = "";
+
+	function handleSelectChange(event) {
+		selectedOption = event.target.value;
+	}
+
+	let reputationTokenAmount;
+	let reputationRegister;
+	let reputationProof;
+	let data_amount_free;
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={dialog}
+	on:close={() => (showModal = false)}
+	on:click|self={() => dialog.close()}
+>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div on:click|stopPropagation>
+		<h2 class="modal-title" id="generateReputationLabel">Generate new reputation proof</h2>
+		<hr />
+		<form id="reputationForm">
+		  <div class="mb-3">
+			<label class="form-label">Choose an option</label>
+			<select class="form-select" bind:value={selectedOption} on:change={handleSelectChange}>
+			  <option></option>
+			  <option value="new">A new one</option>
+			  <option value="another">From another reputation prove</option>
+			</select>
+		  </div>
+		  <div>
+			  {#if selectedOption === "new"}
+				  <div class="mb-3">
+					  <label for="reputationToken" class="form-label">Token amount<span class="required">*</span></label>
+					  <input type="number" class="form-control" bind:value={reputationTokenAmount}/>
+				  </div>
+			  {/if}
+			  {#if selectedOption === "another"}
+				  <div class="mb-3">
+					  <label class="form-label">Reputation proof</label>
+					  <select class="form-select" bind:value={reputationProof}>' +
+						  <option></option>
+					  </select>
+				  </div>
+				  <div class="mb-3">
+					  <label for="reputationToken" class="form-label">Token amount<span class="required">*</span></label>
+					  <input type="number" class="form-control" bind:value={reputationTokenAmount} max="{data_amount_free}"/>
+				  </div>
+			  {/if}
+			  {#if selectedOption !== ""}
+				  <div class="mb-3">
+					  <label for="reputationRegister" class="form-label">Object to assign reputation</label>
+					  <input type="text" class="form-control" bind:value={reputationRegister}/>
+				  </div>
+			  {/if}
+		  </div>
+		</form>
+		<hr />
+		<!-- svelte-ignore a11y-autofocus -->
+		<div class="row">
+			<!-- <button autofocus on:click={() => dialog.close()}>x</button> -->
+			<button on:click={() => generate_reputation_proof(
+					selectedOption==="new",
+					reputationProof,
+					reputationTokenAmount
+			)}>Generate proof</button>
+		</div>
+	</div>
+</dialog>
+
+<style>
+	dialog {
+		max-width: 32em;
+		border-radius: 0.2em;
+		border: none;
+		padding: 0;
+	}
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.3);
+	}
+	dialog > div {
+		padding: 1em;
+	}
+	dialog[open] {
+		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
+	@keyframes zoom {
+		from {
+			transform: scale(0.95);
+		}
+		to {
+			transform: scale(1);
+		}
+	}
+	dialog[open]::backdrop {
+		animation: fade 0.2s ease-out;
+	}
+	@keyframes fade {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+	button {
+		display: block;
+	}
+</style>
