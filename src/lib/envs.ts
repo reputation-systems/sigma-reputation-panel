7069f497386643cf19b8ca8955ab7d47b7a86e40
jossemii <jossemii@proton.me>
2023-11-16 20:20:44 +0100

    some small envs improvement

--- src/lib/envs.ts
+++ src/lib/envs.ts
@@ -2,11 +2,9 @@ import { compile } from "@fleet-sdk/compiler";
 import { ErgoAddress, Network } from "@fleet-sdk/core";
 import { sha256, hex } from "@fleet-sdk/crypto";
 
-export const explorer_uri = "https://api.ergoplatform.com";
+export const explorer_uri = "https://api-testnet.ergoplatform.com";
 
-let contract = "{proveDlog(decodePoint(SELF.R7[Coll[Byte]].get)) && OUTPUTS.forall({(x: Box) => x.propositionBytes == SELF.propositionBytes})}";
-// let contract = "{proveDlog(CONTEXT.preHeader.minerPk)}"
-
+let contract = "{proveDlog(decodePoint(SELF.R7[Coll[Byte]].get)) && sigmaProp(OUTPUTS.forall({(box1: Box) => box1.propositionBytes == SELF.propositionBytes }))}";
 let ergoTree = compile(contract, {version: 1})
 
 let ertoTreeAddress = ErgoAddress.fromErgoTree(ergoTree.toHex(), Network.Testnet).toString()
