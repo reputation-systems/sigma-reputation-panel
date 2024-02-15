c520f585fbaed303808217dcebc1216ff86aeba2
jossemii <jossemii@proton.me>
2024-02-15 20:04:30 +0100

    ergo tree template

--- src/lib/envs.ts
+++ src/lib/envs.ts
@@ -7,8 +7,8 @@ export const explorer_uri = "https://api-testnet.ergoplatform.com";
 let contract = "{proveDlog(decodePoint(SELF.R7[Coll[Byte]].get)) && sigmaProp(OUTPUTS.forall({(box1: Box) => box1.propositionBytes == SELF.propositionBytes }))}";
 let ergoTree = compile(contract, {version: 1})
 
-let ertoTreeAddress = ErgoAddress.fromErgoTree(ergoTree.template.toHex(), Network.Testnet).toString()
-let ergoTreeHash = hex.encode(sha256(ergoTree.toBytes()))
+let ergoTreeAddress = ErgoAddress.fromErgoTree(ergoTree.toHex(), Network.Testnet).toString()
+let ergoTreeHash = hex.encode(sha256(ergoTree.template.toBytes()))
 
 export const ergo_tree_hash = ergoTreeHash
-export const ergo_tree_address = ertoTreeAddress;
+export const ergo_tree_address = ergoTreeAddress;
