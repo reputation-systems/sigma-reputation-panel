f511b0e481f43493fb91bd2f763499e55c483d90
jossemii <jossemii@proton.me>
2024-02-28 20:18:12 +0100

    show all boxes on the graph

--- src/routes/Menu.svelte
+++ src/routes/Menu.svelte
@@ -2,33 +2,13 @@
     import { ControlButton } from "@xyflow/svelte";
     import Modal from "./Modal.svelte";
 
-    export let connected = false;
+    export let connected: boolean = false;
     let showModal = false;
-
-    async function connectNautilus() {
-        if (typeof ergoConnector !== 'undefined') {
-        const nautilus = ergoConnector.nautilus;
-        if (nautilus) {
-            connected = await nautilus.connect();
-            if (connected) {
-                console.log('Connected!');
-            } else {
-                alert('Not connected!');
-            }
-        } else {
-            alert('Nautilus Wallet is not active');
-        }
-        } else {
-        alert('No wallet available');
-        }
-    }
-
-    if (!connected) { connectNautilus() }
 </script>
 
 <!-- svelte-ignore missing-declaration -->
 {#if connected}
-<ControlButton on:click={() => (showModal = true)}><i class="fas fa-cog" style="color: black;"></i></ControlButton>
+    <ControlButton on:click={() => (showModal = true)}><i class="fas fa-cog" style="color: black;"></i></ControlButton>
 {/if}
 
 <Modal bind:showModal />
