d079fe6b2ee550bc8f7582d1462ec5fc340d6a08
jossemii <jossemii@proton.me>
2024-02-28 17:45:17 +0100

    added svelteflow

--- src/routes/Graph.svelte
+++ src/routes/Graph.svelte
@@ -1,90 +1,60 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import * as d3 from 'd3';
-    import { updateReputationProofList } from '$lib/unspent_proofs';
-    import { ergo_tree_hash, explorer_uri } from '$lib/envs';
-    import type { RPBox, ReputationProof } from '$lib/ReputationProof';
-  
-    let svgElement: SVGSVGElement;
-    let unspend_reputation_proofs: RPBox[] = [];
-
-    async function fetchReputationProofs() {
-		try {
-			console.log('Searching for boxes....')
-			const data = await updateReputationProofList(explorer_uri, ergo_tree_hash, ergo);
-			unspend_reputation_proofs = data.map((e: ReputationProof) => e.current_boxes[0]);;
-            console.log(unspend_reputation_proofs)
-            build_graph();
-		} catch (error) {
-			console.error(error);
-		}
-    }
-
-    function build_graph() {
-        const svg = d3.select(svgElement);
-        const width = 400, height = 300;
-  
-        const nodes = unspend_reputation_proofs.map(proof => ({
-            id: proof.box_id,
-            name: proof.box_id.slice(0, 10) + " - " +proof.token_id.slice(0, 10) + "  ("+proof.token_amount+")"
-        }));
-    
-        /*const links = [
-            { source: 1, target: 2 },
-            { source: 2, target: 3 },
-            { source: 3, target: 1 },
-        ];*/  const links = []
-    
-        const simulation = d3.forceSimulation(nodes)
-            .force("link", d3.forceLink(links).id(d => d.id))
-            .force("charge", d3.forceManyBody())
-            .force("center", d3.forceCenter(width / 2, height / 2));
-    
-        const link = svg.append("g")
-            .attr("class", "links")
-            .selectAll("line")
-            .data(links)
-            .enter().append("line")
-            .attr("stroke-width", 2);
-    
-        const node = svg.append("g")
-            .attr("class", "nodes")
-            .selectAll("circle")
-            .data(nodes)
-            .enter().append("circle")
-            .attr("r", 10)
-            .attr("fill", "blue");
-    
-        const labels = svg.append("g")
-            .attr("class", "labels")
-            .selectAll("text")
-            .data(nodes)
-            .enter().append("text")
-            .text(d => d.name)
-            .attr("x", 15)
-            .attr("y", 4);
-    
-        simulation.on("tick", () => {
-            link
-                .attr("x1", d => d.source.x)
-                .attr("y1", d => d.source.y)
-                .attr("x2", d => d.target.x)
-                .attr("y2", d => d.target.y);
-    
-            node
-                .attr("cx", d => d.x)
-                .attr("cy", d => d.y);
-    
-            labels
-                .attr("x", d => d.x + 15)
-                .attr("y", d => d.y + 4);
-        });
-    }
-  
-    onMount(() => {
-        fetchReputationProofs();
-    });
+    import { writable } from 'svelte/store';
+    import {
+      SvelteFlow,
+      Controls,
+      Background,
+      BackgroundVariant,
+      MiniMap
+    } from '@xyflow/svelte';
+   
+    // 👇 this is important! You need to import the styles for Svelte Flow to work
+    import '@xyflow/svelte/dist/style.css';
+   
+    // We are using writables for the nodes and edges to sync them easily. When a user drags a node for example, Svelte Flow updates its position.
+    const nodes = writable([
+      {
+        id: '1',
+        type: 'input',
+        data: { label: 'Input Node' },
+        position: { x: 0, y: 0 }
+      },
+      {
+        id: '2',
+        type: 'default',
+        data: { label: 'Node' },
+        position: { x: 0, y: 150 }
+      }
+    ]);
+   
+    // same for edges
+    const edges = writable([
+      {
+        id: '1-2',
+        type: 'default',
+        source: '1',
+        target: '2',
+        label: 'Edge Text'
+      }
+    ]);
+   
+    const snapGrid = [25, 25];
   </script>
-  
-  <svg bind:this={svgElement} width="100%" height="100%"></svg>
-  
+   
+  <!--
+  👇 By default, the Svelte Flow container has a height of 100%.
+  This means that the parent container needs a height to render the flow.
+  -->
+  <div style:height="500px">
+    <SvelteFlow
+      {nodes}
+      {edges}
+      {snapGrid}
+      fitView
+      on:nodeclick={(event) => console.log('on node click', event.detail.node)}
+    >
+      <Controls />
+      <Background variant={BackgroundVariant.Dots} />
+      <MiniMap />
+    </SvelteFlow>
+  </div>
