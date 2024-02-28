3d7a6c92dc5b2ae73420c3ff0d53303baefbba26
jossemii <jossemii@proton.me>
2024-02-28 09:01:37 +0100

    Updates

--- reputation_proof.scala
+++ reputation_proof.scala
@@ -1,28 +1,19 @@
 /**
 *
-* Reputation Proof  -  
-    
-
+* Reputation Proof
     R5     -> Pointer to the object type.                 ex: Box, git repo, url
     R6     -> Pointer to the object to assign reputation: ex: fjdfklj4314j3lk, https...
     R7     -> owner public key
-
-
-    ** Where Box is an Ergo box.
-
-    TODO: Limit the number of possible tokens to one
 */
-    // An optional object where the proof assign it's reputation 
-    // (it could be different types of data, like other Reputation systems, urls, git repositories, etc).
-    //
-    //
-// ENVIROMENT_VARIABLE_THAT_ALLOWS_SPEND_OR_NOT &&
-// SELF.R6[Coll[Byte]].isDefined != true &&
-    // 
-    //
-    // The proof's creator (or, at least, the one chosen by the box's creator) can spend the tokens
-    // Owner's public key.  Without it, the box can't be expended. 
-proveDlog(SELF.R7[GroupElement].get) &&
-    //
-    // Assign them ONLY to other reputation proofs.
-sigmaProp(OUTPUTS.exists({(x: Box) => x.propositionBytes == SELF.propositionBytes}))
+{
+    proveDlog(SELF.R7[GroupElement].get) &&
+    sigmaProp(SELF.tokens.size == 1) &&
+    sigmaProp(OUTPUTS.forall { (x: Box) =>
+      !(x.tokens.exists { (token: (Coll[Byte], Long)) => token._1 == SELF.tokens(0)._1 }) ||
+      (
+        x.R7[GroupElement].get == SELF.R7[GroupElement].get &&
+        x.tokens.size == 1 &&
+        x.propositionBytes == SELF.propositionBytes
+      )
+    })  
+}
