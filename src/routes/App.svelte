3f6eaf0598018157d82e2c91f2b8843fad8edc30
jossemii <jossemii@proton.me>
2024-02-29 17:36:09 +0100

    only rep.proofs

--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -1,13 +1,13 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 <script lang="ts">
     import { writable } from 'svelte/store';
-    import { SvelteFlow, Background, type Node, Controls, MiniMap, Position } from '@xyflow/svelte';
+    import { SvelteFlow, Background, type Node, Controls, MiniMap, Position, type Edge, Panel, ControlButton } from '@xyflow/svelte';
   
     import '@xyflow/svelte/dist/style.css';
     import { updateReputationProofList } from '$lib/unspent_proofs';
     import { ergo_tree_hash, explorer_uri } from '$lib/envs';
     import type { ReputationProof } from '$lib/ReputationProof';
-    import ELK from 'elkjs/lib/elk.bundled.js';
+    import dagre from '@dagrejs/dagre';
 
     import Menu from './Menu.svelte';
     import Header from './Header.svelte';
@@ -46,55 +46,87 @@
     }
     
     $: if (connected) { fetchReputationProofs(); }
-  
-    const elk = new ELK();
-    // Elk has a *huge* amount of options to configure. To see everything you can
-    // tweak check out:
-    //
-    // - https://www.eclipse.org/elk/reference/algorithms.html
-    // - https://www.eclipse.org/elk/reference/options.html
-    const elkOptions = {
-      'elk.algorithm': 'layered',
-      'elk.layered.spacing.nodeNodeBetweenLayers': '100',
-      'elk.spacing.nodeNode': '80'
-    };
+
+    const dagreGraph = new dagre.graphlib.Graph();
+    dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+    const nodeWidth = 172;
+    const nodeHeight = 36;
+
+    function getLayoutedElements(nodes: Node[], edges: Edge[], direction = 'TB') {
+      const isHorizontal = direction === 'LR';
+      dagreGraph.setGraph({ rankdir: direction });
+
+      nodes.forEach((node) => {
+        dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+      });
+
+      edges.forEach((edge) => {
+        dagreGraph.setEdge(edge.source, edge.target);
+      });
+
+      dagre.layout(dagreGraph);
+
+      nodes.forEach((node) => {
+        const nodeWithPosition = dagreGraph.node(node.id);
+        node.targetPosition = isHorizontal ? Position.Left : Position.Top;
+        node.sourcePosition = isHorizontal ? Position.Right : Position.Bottom;
+
+        node.position = {
+          x: nodeWithPosition.x - nodeWidth / 2,
+          y: nodeWithPosition.y - nodeHeight / 2
+        };
+      });
+
+      return { nodes, edges };
+    }
 
     const nodes = writable<Node[]>([]);
-    const edges = writable([]);
+    const edges = writable<Edge[]>([]);
+
+    function onLayout(direction: string) {
+      const layoutedElements = getLayoutedElements($nodes, $edges, direction);
+
+      $nodes = layoutedElements.nodes;
+      $edges = layoutedElements.edges;
+    }
 
     function build_graph(proofs: ReputationProof[]) {
       $nodes = [];
       $edges = [];
       let _x = 0; let _y = 0;
       proofs.map(p => {
-        /*$nodes.push({
+        $nodes.push({
             id: p.token_id,
-            type: "group",
+            // type: "group",
             sourcePosition: window.innerWidth > window.innerHeight ? Position.Right : Position.Bottom, 
             targetPosition: window.innerWidth > window.innerHeight ? Position.Left : Position.Top,
             data: { label: p.token_id.slice(0, 10) },
             position: { x: _x, y: _y },
-          });*/
-        p.current_boxes.map(b => {
+          });
+        /*p.current_boxes.map(b => {
           const percentage_of_tokens = parseFloat(Number(b.token_amount/p.total_amount * 100).toFixed(3));
           $nodes.push({
             id: b.box_id,
-            // parentNode: b.token_id,
-            // expandParent: true,
+            parentNode: b.token_id,
+            expandParent: true,
             data: { label: b.box_id.slice(0, 10)+" - "+percentage_of_tokens+ "%" }, 
             position: { x: _x, y: _y },
           });
-        });
+        });*/
       });
+      onLayout(window.innerWidth > window.innerHeight ? 'TB' : 'LR', );
     }
   </script>
   
 
   <div style="height:100vh;">
-    <SvelteFlow {nodes} {edges} style="background: rgb(17, 17, 17)" fitView>
+    <SvelteFlow {nodes} {edges} style="background: #1A192B" fitView>
       <Background />
       <Header />
       <Controls>
+        <ControlButton on:click={() => onLayout('TB')}><i class="fas fa-regular fa-ruler-horizontal"></i></ControlButton>
+        <ControlButton on:click={() => onLayout('LR')}><i class="fas fa-regular fa-ruler-vertical"></i></ControlButton>
         <Menu bind:connected/>
       </Controls>
       <MiniMap />
