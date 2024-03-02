482154ba8aa98c9fc41eaf9cba1cbc9a9c8d406c
jossemii <jossemii@proton.me>
2024-03-02 23:28:44 +0100

    fixed https://svelteflow.dev/learn/guides/custom-nodes#suppress-unknown-prop-warnings

--- src/routes/NodeCircleType.svelte
+++ src/routes/NodeCircleType.svelte
@@ -3,6 +3,17 @@
   type $$Props = NodeProps;
 
   export let id: $$Props['id'];
+  export let data: $$Props['data']; data;
+  export let dragHandle: $$Props['dragHandle'] = undefined; dragHandle;
+  export let type: $$Props['type']  = undefined; type;
+  export let selected: $$Props['selected'] = undefined; selected;
+  export let isConnectable: $$Props['isConnectable'] = undefined; isConnectable;
+  export let zIndex: $$Props['zIndex'] = undefined; zIndex;
+  export let width: $$Props['width'] = undefined; width;
+  export let height: $$Props['height'] = undefined; height;
+  export let dragging: $$Props['dragging']; dragging;
+  export let targetPosition: $$Props['targetPosition'] = undefined; targetPosition;
+  export let sourcePosition: $$Props['sourcePosition'] = undefined; sourcePosition;
 
   const connections = useHandleConnections({ nodeId: id, type: 'target' });
 
