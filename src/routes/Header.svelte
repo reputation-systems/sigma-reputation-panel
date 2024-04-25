65d7ddadcf2475cb386ba23dc45ded9416223943
jse07 <jse07@anon.erg>
2024-04-25 19:18:02 +0200

    change title.

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,5 @@
 <div>
-    <h1 style="background-color: aliceblue; display:inline-block;">Welcome to the Ergo Reputation System Panel</h1>
+    <h1 style="background-color: aliceblue; display:inline-block;">Welcome to the Sigma Reputation Panel</h1>
     <a class="github-button" href="https://github.com/celaut-project/ergo-reputation-system" target="_blank">
       <img src="https://cdn.icon-icons.com/icons2/844/PNG/512/Github_icon-icons.com_67091.png" alt="GitHub" width="50" height="50">
     </a>
