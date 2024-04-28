7a1dcc36f426683ba142a8177e5ad6d228326418
jse07 <jse07@anon.erg>
2024-04-28 16:49:56 +0200

    paginate

--- src/lib/unspent_proofs.ts
+++ src/lib/unspent_proofs.ts
@@ -56,77 +56,86 @@ export async function updateReputationProofList(explorer_uri: string, ergo_tree_
             offset: 0,
             limit: 500,
         };
-        
-        const url = explorer_uri+'/api/v1/boxes/unspent/search';
-        const response = await fetch(url + '?' + new URLSearchParams({
-            offset: params.offset.toString(),
-            limit: params.limit.toString(),
-        }), {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-                "ergoTreeTemplateHash": ergo_tree_template_hash,
-                "registers": {
-                    "R4":  stringToRendered(reputation_token_label),
-                   //  "R7":  serializedToRendered(generate_pk_proposition((await ergo.get_change_address())))
-                },
-                "constants": {},
-                "assets": []
-            }),
-        });
+        let proofs = new Map<string, ReputationProof>();
+        let moreDataAvailable = true;
 
-        if (response.ok) {
-            let proofs = new Map<string, ReputationProof>();
-            let json_data = await response.json();
-            json_data.items.forEach((e: ApiBox) => {
-                let token_id = e.assets[0].tokenId;
-                let current_box: RPBox = {
-                        box_id: e.boxId,
-                        token_id: e.assets.length > 0 ? e.assets[0].tokenId : "",
-                        token_amount: e.assets.length > 0 ? Number(e.assets[0].amount) : 0,
-                        box: {
-                            boxId: e.boxId,
-                            value: e.value,
-                            assets: e.assets,
-                            ergoTree: e.ergoTree,
-                            creationHeight: e.creationHeight,
-                            additionalRegisters: Object.entries(e.additionalRegisters).reduce((acc, [key, value]) => {
-                                acc[key] = value.serializedValue;
-                                return acc;
-                            }, {} as {
-                                [key: string]: string;
-                            }),
-                            index: e.index,
-                            transactionId: e.transactionId
-                        }
-                    };
-                
-                if (e.additionalRegisters.R6 !== undefined && e.additionalRegisters.R5 !== undefined) {
-                    current_box.object_type = object_type_by_rendered_value(e.additionalRegisters.R5.renderedValue),
-                    current_box.object_value = e.additionalRegisters.R6.renderedValue;
-                }
-
-                let _reputation_proof: ReputationProof = proofs.has(token_id) 
-                    ? proofs.get(token_id)! 
-                    : {
-                        current_boxes: [], 
-                        token_id: token_id,
-                        number_of_boxes: 0,
-                        total_amount: 0
-                    };
-                _reputation_proof.current_boxes.push(current_box);
-                _reputation_proof.total_amount += current_box.token_amount;
-                _reputation_proof.number_of_boxes += 1;
-                proofs.set(token_id, _reputation_proof);
+        while (moreDataAvailable) {
+            const url = explorer_uri+'/api/v1/boxes/unspent/search';
+            const response = await fetch(url + '?' + new URLSearchParams({
+                offset: params.offset.toString(),
+                limit: params.limit.toString(),
+            }), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                    "ergoTreeTemplateHash": ergo_tree_template_hash,
+                    "registers": {
+                        "R4":  stringToRendered(reputation_token_label),
+                       //  "R7":  serializedToRendered(generate_pk_proposition((await ergo.get_change_address())))
+                    },
+                    "constants": {},
+                    "assets": []
+                }),
             });
-            return Array.from(proofs.values());
-        } 
-        else {
-            console.error('Error al realizar la solicitud POST');
-            return [];
+
+            if (response.ok) {
+                let json_data = await response.json();
+                if (json_data.items.length == 0) {
+                    moreDataAvailable = false;
+                    break;
+                }
+                json_data.items.forEach((e: ApiBox) => {
+                    let token_id = e.assets[0].tokenId;
+                    let current_box: RPBox = {
+                            box_id: e.boxId,
+                            token_id: e.assets.length > 0 ? e.assets[0].tokenId : "",
+                            token_amount: e.assets.length > 0 ? Number(e.assets[0].amount) : 0,
+                            box: {
+                                boxId: e.boxId,
+                                value: e.value,
+                                assets: e.assets,
+                                ergoTree: e.ergoTree,
+                                creationHeight: e.creationHeight,
+                                additionalRegisters: Object.entries(e.additionalRegisters).reduce((acc, [key, value]) => {
+                                    acc[key] = value.serializedValue;
+                                    return acc;
+                                }, {} as {
+                                    [key: string]: string;
+                                }),
+                                index: e.index,
+                                transactionId: e.transactionId
+                            }
+                        };
+                    
+                    if (e.additionalRegisters.R6 !== undefined && e.additionalRegisters.R5 !== undefined) {
+                        current_box.object_type = object_type_by_rendered_value(e.additionalRegisters.R5.renderedValue),
+                        current_box.object_value = e.additionalRegisters.R6.renderedValue;
+                    }
+                    let _reputation_proof: ReputationProof = proofs.has(token_id) 
+                        ? proofs.get(token_id)! 
+                        : {
+                            current_boxes: [], 
+                            token_id: token_id,
+                            number_of_boxes: 0,
+                            total_amount: 0
+                        };
+                    _reputation_proof.current_boxes.push(current_box);
+                    _reputation_proof.total_amount += current_box.token_amount;
+                    _reputation_proof.number_of_boxes += 1;
+                    proofs.set(token_id, _reputation_proof);
+                });
+                params.offset += params.limit;
+            } 
+            else {
+                console.error('Error al realizar la solicitud POST');
+                return [];
+            }
         }
+        const arr = Array.from(proofs.values());
+        console.log("Total proofs -> ", arr.length)
+        return arr;
     } catch (error) {
         console.error('Error al procesar la solicitud POST:', error);
         return [];
