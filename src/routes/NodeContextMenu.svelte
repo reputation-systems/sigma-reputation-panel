7d2f37533cf9efe93dcef1d16a165eb87e2cffd0
jse07 <jse07@anon.erg>
2024-04-30 19:53:59 +0200

    redesign Ok.

--- src/routes/NodeContextMenu.svelte
+++ src/routes/NodeContextMenu.svelte
@@ -1,99 +1,201 @@
 <script lang="ts">
-    import type { ReputationProof } from "$lib/ReputationProof";
+
+  import FormModal from "./CreateProofModal.svelte";
+  import SettingModal from "./Settings.svelte";
+  import Search from "./Search.svelte";
+  
+  import type { ReputationProof } from "$lib/ReputationProof";
 
   
-    export let onClick: () => void;
-    export let proof: ReputationProof|null;
-    export let top: number | undefined;
-    export let left: number | undefined;
-    export let right: number | undefined;
-    export let bottom: number | undefined;
-  
+  export let onClick: () => void;
+  export let proof: ReputationProof|null;
+  export let top: number | undefined;
+  export let left: number | undefined;
+  export let right: number | undefined;
+  export let bottom: number | undefined;
 
-    let local_id: string = proof ? proof.token_id.slice(0, 10) : ""; 
-  </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<div
-style="top: {top}px; left: {left}px; right: {right}px; bottom: {bottom}px;"
-class="context-menu"
-on:click={onClick}
->
+  let local_id: string = proof ? proof.token_id.slice(0, 10) : ""; 
+
+  // pos is cursor position when right click occur
+  let pos = { x: 0, y: 0 }
+  // menu is dimension (height and width) of context menu
+  let menu = { h: 0, y: 0 }
+  // browser/window dimension (height and width)
+  let browser = { h: 0, y: 0 }
+  let showMenu = false;
+  let showSearch = false;
+  let showForm = false;
+  let showSetting = false;
+  export let setter: CallableFunction;
+
+
+  function rightClickContextMenu(e){
+      showMenu = true
+      browser = {
+          w: window.innerWidth,
+          h: window.innerHeight
+      };
+      pos = {
+          x: e.clientX,
+          y: e.clientY
+      };
+      // If bottom part of context menu will be displayed
+      // after right-click, then change the position of the
+      // context menu. This position is controlled by `top` and `left`
+      // at inline style. 
+      // Instead of context menu is displayed from top left of cursor position
+      // when right-click occur, it will be displayed from bottom left.
+      if (browser.h -  pos.y < menu.h)
+          pos.y = pos.y - menu.h
+      if (browser.w -  pos.x < menu.w)
+          pos.x = pos.x - menu.w
+  }
+  function onPageClick(e){
+      // To make context menu disappear when
+      // mouse is clicked outside context menu
+      showMenu = false;
+  }
+  function getContextMenuDimension(node){
+      // This function will get context menu dimension
+      // when navigation is shown => showMenu = true
+      let height = node.offsetHeight
+      let width = node.offsetWidth
+      menu = {
+          h: height,
+          w: width
+      }
+  }
+  function updateItem(){
+      // Implement the logic for updating here
+      console.log("update item")
+  }
+  let menuItems = [
+      {
+          'name': 'updateItem',
+          'onClick': updateItem,
+          'displayText': "Update",
+          'class': 'fa-solid fa-pencil-alt'
+      }
+  ]
+
+</script>
+<svelte:head>
+  <!-- You can change icon sets according to your taste. Change `class` value in `menuItems` above to represent your icons. -->
+  <!-- <link rel="stylesheet" href="/icon/css/mfglabs_iconset.css"> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</svelte:head>
+
+{#if showMenu}
+<nav use:getContextMenuDimension style="position: absolute; top:{pos.y}px; left:{pos.x}px">
   <div class="navbar" id="navbar">
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <p style="margin: 0.5em;">
-      <!-- svelte-ignore a11y-missing-attribute -->
-      Proof <a>{local_id}</a>
-      <!-- svelte-ignore a11y-missing-attribute -->
-      <br>
-      Network <a>Ergo Platform</a>
-      <br/><br/>
-      {#if proof && proof.tag}
-        {#each proof.tag.split(';') as tag}
-          <span style={ (proof && proof.can_be_spend) ? "background-color: #007bff" : "background-color: #ff4500" } 
-            class="tag tag-hover">
-            { tag }
-          </span>
-        {/each}
-      {/if}
-    </p>    
-</div>
-</div>
-  
+    <div class="info-block">
+      <p>
+          <span class="label">Proof:</span> <a>{local_id}</a><br>
+          <span class="label">Network:</span> <a>Ergo Platform</a><br>
+          {#if proof}
+              <span class="label">Tags:</span> <a>{proof.tag}</a>
+          {/if}
+      </p>
+  </div>  
+      <ul>
+          {#each menuItems as item}
+              {#if item.name == "hr"}
+                  <hr>
+              {:else}
+                  <li><button on:click={item.onClick}><i class={item.class}></i>{item.displayText}</button></li>
+              {/if}
+          {/each}
+      </ul>
+  </div>
+</nav>
+{/if}
+
+<svelte:window on:contextmenu|preventDefault={rightClickContextMenu} on:click={onPageClick} />
+
+<FormModal bind:showModal={showForm} />
+<Search bind:showSearch />
+<SettingModal bind:showModal={showSetting} bind:setter />
+
+
 <style>
-.context-menu {
-    position: absolute;
-    z-index: 10;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    border: 1px solid #007bff;
-    animation: fadeIn 0.3s;
-    background-color: white;
-}
+    .info-block {
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        padding: 10px;
+        margin-bottom: 10px;
+        background-color: #f9f9f9;
+    }
 
-@keyframes fadeIn {
-    from { opacity: 0; }
-    to { opacity: 1; }
-}
+    .info-block p {
+        margin: 0;
+        font-size: 16px;
+        line-height: 1.6;
+    }
 
-.navbar {
-    display: flex;
-    background-color: #ffffff; 
-    border-radius: 10px;
-    overflow: hidden;
-    flex-direction: column;
-    padding: 1em;
-    align-items: center;
-    justify-content: center;
-    width: auto;
-    max-width: 300px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
-}
+    .label {
+        font-weight: bold;
+    }
 
-.navbar a {
-    color: #000000; 
-    text-decoration: none;
-    margin-bottom: 0.5em;
-    font-size: 1.1em;
-    font-weight: bold;
-}
+    .info-block a {
+        color: #052241;
+        text-decoration: none;
+    }
 
-.tag {
-    display: inline-block;
-    padding: 6px 12px;
-    margin: 4px;
-    color: white;
-    border: none;
-    border-radius: 12px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-    transition: transform 0.3s ease, box-shadow 0.3s ease;
-    font-size: 0.85em;
-}
-
-.tag:hover {
-    transform: translateY(-2px);
-    box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
-}
-
-
+  * {
+      padding: 0;
+      margin: 0;
+  }
+  .navbar{
+      display: inline-flex;
+      border: 1px #999 solid;
+      width: 170px;
+      background-color: #fff;
+      border-radius: 10px;
+      overflow: hidden;
+      flex-direction: column;
+  }
+  .navbar ul{
+      margin: 6px;
+  }
+  ul li{
+      display: block;
+      list-style-type: none;
+      width: 1fr;
+  }
+  ul li button{
+      font-size: 1rem;
+      color: #222;
+      width: 100%;
+      height: 30px;
+      text-align: left;
+      border: 0px;
+      background-color: #fff;
+  }
+  ul li button:hover{
+      color: #000;
+      text-align: left;
+      border-radius: 5px;
+      background-color: #eee;
+  }
+  ul li button i{
+      padding: 0px 15px 0px 10px;
+  }
+  ul li button i.fa-square{
+      color: #fff;
+  }
+  ul li button:hover > i.fa-square{
+      color: #eee;
+  }
+  ul li button:hover > i.warning{
+      color: crimson;
+  }
+  :global(ul li button.info:hover){
+      color: navy;
+  }
+  hr{
+      border: none;
+      border-bottom: 1px solid #ccc;
+      margin: 5px 0px;
+  }
 </style>
