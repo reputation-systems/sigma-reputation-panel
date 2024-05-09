97bd91d1d6c56ddc7d6222de77a389f95ac0b8d9
jse07 <jse07@anon.erg>
2024-05-09 21:24:37 +0200

    compute function on TS.

--- src/lib/ReputationProof.ts
+++ src/lib/ReputationProof.ts
@@ -44,3 +44,28 @@ export enum Network {
     BitcoinTestnet = "btc-testnet",
     BitcoinMainnet = "btc-mainnet"
 }
+
+export function compute(proofs: Map<string, ReputationProof>, proof: ReputationProof, object_type: ObjectType, object_value: string): number {
+    return proof.current_boxes.reduce((total, box) => {
+        const proportion = box.token_amount / proof.total_amount;
+        const boxReputation = proportion * computePointerReputation(proofs, box, object_type, object_value);
+        return total + boxReputation;
+    }, 0);
+}
+
+function computePointerReputation(proofs: Map<string, ReputationProof>, box: RPBox, object_type: ObjectType, object_value: string): number {
+    switch (box.object_type) {
+        case ObjectType.ProofByToken:
+            if (object_type === ObjectType.ProofByToken && box.object_value === object_value) {
+                return 1.00;
+            } else if (box.object_value && proofs.has(box.object_value)) {
+                return compute(proofs, proofs.get(box.object_value)!, object_type, object_value);
+            } else {
+                return 0.00;
+            }
+        case ObjectType.PlainText:
+            return object_type === ObjectType.PlainText && object_value === box.object_value ? 1.00 : 0.00;
+        default:
+            return 0.00;
+    }
+}
