9b98b657a4e440e4138d97e7ef7e1ecb7af63c22
0xf965 <0xf965@non.exists>
2024-09-09 19:41:30 +0200

    changed contract

--- src/lib/envs.ts
+++ src/lib/envs.ts
@@ -5,22 +5,25 @@ import { sha256, hex } from "@fleet-sdk/crypto";
 export const explorer_uri = "https://api-testnet.ergoplatform.com";
 
 let contract = `{
-  proveDlog(SELF.R7[GroupElement].get) &&
+  SELF.R7[SigmaProp].get &&
   sigmaProp(SELF.tokens.size == 1) &&
   sigmaProp(OUTPUTS.forall { (x: Box) =>
     !(x.tokens.exists { (token: (Coll[Byte], Long)) => token._1 == SELF.tokens(0)._1 }) ||
     (
-      x.R7[GroupElement].get == SELF.R7[GroupElement].get &&
+      x.R7[SigmaProp].get == SELF.R7[SigmaProp].get &&
       x.tokens.size == 1 &&
       x.propositionBytes == SELF.propositionBytes &&
       (x.R8[Boolean].get == false || x.R8[Boolean].get == true)
     )
   })
 }`;
-let ergoTree = compile(contract, {version: 1})
+let ergoTree = compile(contract, { version: 1 });
 
-let ergoTreeAddress = ErgoAddress.fromErgoTree(ergoTree.toHex(), Network.Testnet).toString()
-let ergoTreeHash = hex.encode(sha256(ergoTree.template.toBytes()))
+let ergoTreeAddress = ErgoAddress.fromErgoTree(
+  ergoTree.toHex(),
+  Network.Testnet,
+).toString();
+let ergoTreeHash = hex.encode(sha256(ergoTree.template.toBytes()));
 
-export const ergo_tree_hash = ergoTreeHash
+export const ergo_tree_hash = ergoTreeHash;
 export const ergo_tree_address = ergoTreeAddress;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,60 +1,82 @@
-import { ErgoAddress, SBool, SByte, SColl, SConstant, SGroupElement } from "@fleet-sdk/core";
+import {
+  ErgoAddress,
+  SBool,
+  SByte,
+  SColl,
+  SConstant,
+  SGroupElement,
+} from "@fleet-sdk/core";
 import { stringToBytes } from "@scure/base";
 import { connected } from "./store";
 import { get } from "svelte/store";
 
 export function serializedToRendered(serializedValue: string): string {
-    // Assuming the serialized value always starts with a pattern to strip (e.g., '0e')
-    const patternToStrip = '0e';
-    if (serializedValue.startsWith(patternToStrip)) {
-        // Remove the pattern and return the hex string
-        return serializedValue.substring(patternToStrip.length).substring(2);
-    } else {
-        // If the pattern does not exist at the start, return the original string
-        return serializedValue.substring(2);
-    }
+  // Assuming the serialized value always starts with a pattern to strip (e.g., '0e')
+  const patternToStrip = "0e";
+  if (serializedValue.startsWith(patternToStrip)) {
+    // Remove the pattern and return the hex string
+    return serializedValue.substring(patternToStrip.length).substring(2);
+  } else {
+    // If the pattern does not exist at the start, return the original string
+    return serializedValue.substring(2);
+  }
 }
 
 export function hexToUtf8(hexString: string): string | null {
-    try {
-        if (hexString.length % 2 !== 0) {
-        console.log('The hexadecimal string has an odd length.');
-        return null;
-        }
-    
-        // Convierte la cadena hexadecimal a un array de bytes
-        const byteArray = new Uint8Array(hexString.match(/.{1,2}/g)!.map(byte => parseInt(byte, 16)));
-    
-        // Crea un nuevo TextDecoder para convertir el array de bytes a una cadena UTF-8
-        const decoder = new TextDecoder('utf-8');
-        const utf8String = decoder.decode(byteArray);
-    
-        return utf8String;
-    } catch {
-        console.log("hex to utf-8 error with ", hexString, typeof(hexString))
-        return null;
+  try {
+    if (hexString.length % 2 !== 0) {
+      console.log("The hexadecimal string has an odd length.");
+      return null;
     }
+
+    // Convierte la cadena hexadecimal a un array de bytes
+    const byteArray = new Uint8Array(
+      hexString.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16)),
+    );
+
+    // Crea un nuevo TextDecoder para convertir el array de bytes a una cadena UTF-8
+    const decoder = new TextDecoder("utf-8");
+    const utf8String = decoder.decode(byteArray);
+
+    return utf8String;
+  } catch {
+    console.log("hex to utf-8 error with ", hexString, typeof hexString);
+    return null;
   }
+}
 
 export function generate_pk_proposition(wallet_pk: string): string {
-    const pk = ErgoAddress.fromBase58(wallet_pk).getPublicKeys()[0];
-    const encodedProp = SGroupElement(pk);
-    return encodedProp.toHex();
+  const pk = ErgoAddress.fromBase58(wallet_pk).getPublicKeys()[0];
+  const encodedProp = SGroupElement(pk);
+  let hex = encodedProp.toHex();
+  return "08cd" + hex.slice(2);
 }
 
 export function stringToSerialized(value: string): string {
-    return SConstant(SColl(SByte, stringToBytes('utf8', value)));
+  return SConstant(SColl(SByte, stringToBytes("utf8", value)));
 }
 
 export function booleanToSerializer(value: boolean): string {
-    return SConstant(SBool(value));
+  return SConstant(SBool(value));
 }
 
 export function stringToRendered(value: string): string {
-    return serializedToRendered(stringToSerialized(value));
+  return serializedToRendered(stringToSerialized(value));
 }
 
-export async function check_if_r7_is_local_addr(value: string): Promise<boolean> {
-    if (!get(connected)) return false;
-    return stringToRendered(generate_pk_proposition((await ergo.get_change_address()))).substring(4,) === stringToRendered(value);
+export async function check_if_r7_is_local_addr(
+  value: string,
+): Promise<boolean> {
+  if (!get(connected)) return false;
+  console.log(
+    stringToRendered(
+      generate_pk_proposition(await ergo.get_change_address()),
+    ).substring(4),
+  );
+  console.log(stringToRendered(value));
+  return (
+    stringToRendered(
+      generate_pk_proposition(await ergo.get_change_address()),
+    ).substring(4) === stringToRendered(value)
+  );
 }
