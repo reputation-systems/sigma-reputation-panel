b46484a7e0d26f6bc9c5161fa52aafdcd0d086bd
0xf965 <thisemaildoesnotexists@anon.erg>
2024-12-25 17:27:08 +0100

    updated contract

--- src/lib/envs.ts
+++ src/lib/envs.ts
@@ -5,17 +5,17 @@ import { sha256, hex } from "@fleet-sdk/crypto";
 export const explorer_uri = "https://api.ergoplatform.com";
 
 let contract = `{
-  proveDlog(SELF.R7[GroupElement].get) &&
-  sigmaProp(SELF.tokens.size == 1) &&
-  sigmaProp(OUTPUTS.forall { (x: Box) =>
+    SELF.R7[SigmaProp].get &&
+    sigmaProp(SELF.tokens.size == 1) &&
+    sigmaProp(OUTPUTS.forall { (x: Box) =>
     !(x.tokens.exists { (token: (Coll[Byte], Long)) => token._1 == SELF.tokens(0)._1 }) ||
     (
-      x.R7[GroupElement].get == SELF.R7[GroupElement].get &&
-      x.tokens.size == 1 &&
-      x.propositionBytes == SELF.propositionBytes &&
-      (x.R8[Boolean].get == false || x.R8[Boolean].get == true)
+        x.R7[SigmaProp].get == SELF.R7[SigmaProp].get &&
+        x.tokens.size == 1 &&
+        x.propositionBytes == SELF.propositionBytes &&
+        (x.R8[Boolean].get == false || x.R8[Boolean].get == true)
     )
-  })
+    })
 }`;
 let ergoTree = compile(contract, {version: 1})
 
