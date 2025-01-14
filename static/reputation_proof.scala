c9bd1be40643912bfc035ce48fcbd743638eb6a0
0xf965 <thisemaildoesnotexists@anon.erg>
2025-01-14 20:47:13 +0100

    fixed .scala.

--- static/reputation_proof.scala
+++ static/reputation_proof.scala
@@ -7,15 +7,15 @@
     R8     -> Polarization
 */
 {
-    proveDlog(SELF.R7[GroupElement].get) &&
+    SELF.R7[SigmaProp].get &&
     sigmaProp(SELF.tokens.size == 1) &&
     sigmaProp(OUTPUTS.forall { (x: Box) =>
-      !(x.tokens.exists { (token: (Coll[Byte], Long)) => token._1 == SELF.tokens(0)._1 }) ||
-      (
-        x.R7[GroupElement].get == SELF.R7[GroupElement].get &&
+    !(x.tokens.exists { (token: (Coll[Byte], Long)) => token._1 == SELF.tokens(0)._1 }) ||
+    (
+        x.R7[SigmaProp].get == SELF.R7[SigmaProp].get &&
         x.tokens.size == 1 &&
         x.propositionBytes == SELF.propositionBytes &&
         (x.R8[Boolean].get == false || x.R8[Boolean].get == true)
-      )
+    )
     })
 }
